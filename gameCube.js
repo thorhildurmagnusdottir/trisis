947e426c5c122c73449e1b9bd4360dcc4e231f15
thorhildurmagnusdottir <thorhildurmagnusdottir@gmail.com>
2015-03-26 13:39:45 +0000

    bjo til hlut i gameCube.js

--- gameCube.js
+++ gameCube.js
@@ -2,6 +2,18 @@
  * Created by thorhildur on 24.3.2015.
  */
 drawGameCube = function(){
-    var gameCubeColor = "invert";
+    var gameCubeColor = 10;
+    colorCube(gameCubeColor);
 //    render code for the game cube
+};
+var gameCube = {
+};
+function Cube(){
+    this.pos = [];
+}
+Cube.prototype = {
+    constructor: Cube,
+    move:function(){
+        this.pos.push("i");
+    }
 };
